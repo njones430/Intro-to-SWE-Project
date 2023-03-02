37fa5296dca5df3dcc52b9dfb2d92306f72b4e9e
Snaphu1 <87694809+Snaphu1@users.noreply.github.com>
2023-03-01 23:47:43 -0500

    Update constants_test.go

--- FullStack/config/constants_test.go
+++ FullStack/config/constants_test.go
@@ -1,40 +1,29 @@
-package constants
+package config
 
 import (
-  "testing"
-  "net/http"
-  "net/http/httptest"
+	"testing"
+
+
 )
 
-type Tests struct {
-  name String
-  server *httptest.Server
-  response *oauth2.Config
-  expectedError error
+func TestSetupConfig(t *testing.T) {
+  got := SetupConfig().ClientID;
+  want :=  "612052932443-ns3e5hrhjtcleqkotlnk8lfo1cq6p3f7.apps.googleusercontent.com"
+  if got != want {
+    t.Errorf("got %s, wanted %s", got, want)
 }
-
-func TestConstants(t * Testing.T) {
-  tests := []Tests {
-    {
-      name: "test",
-      server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte('{"ClientID": "612052932443-ns3e5hrhjtcleqkotlnk8lfo1cq6p3f7.apps.googleusercontent.com", 
-                       "ClientSecret": "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",
-                       "RedirectURL":  "http://localhost:3000/google/callback",
-                       "Scopes": "[]string{ "https://www.googleapis.com/auth/userinfo.email",}",
-		                   "Endpoint": "google.Endpoint",
-      })),
-        response: &oauth2.Config {
-          ClientID:     "612052932443-ns3e5hrhjtcleqkotlnk8lfo1cq6p3f7.apps.googleusercontent.com",
-		      ClientSecret: "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",
-		      RedirectURL:  "http://localhost:3000/google/callback",
-		      Scopes: []string{
-			      "https://www.googleapis.com/auth/userinfo.email",
-		      },
-		      Endpoint: google.Endpoint,
-        },
-        expectedError: nil
-    },
-  }
+}
+func TestSetupConfig1(t *testing.T) {
+  got := SetupConfig().ClientSecret;
+  want :=  "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"
+  if got != want {
+    t.Errorf("got %s, wanted %s", got, want)
+}
+}
+func TestSetupConfig2(t *testing.T) {
+  got := SetupConfig().RedirectURL;
+  want :=  "http://localhost:3000/google/callback"
+  if got != want {
+    t.Errorf("got %s, wanted %s", got, want)
+}
 }
