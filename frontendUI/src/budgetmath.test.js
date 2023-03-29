22fbffe1819aad361229c4e6e8e9eaa80677f361
L-Spratt <119978659+L-Spratt@users.noreply.github.com>
2023-03-29 18:12:02 -0400

    Delete budgetmath.test.js

--- frontendUI/src/budgetmath.test.js
+++
@@ -1,6 +0,0 @@
-const math = require('./budgetmath');
-
-test('monthly income calculated properly', () => {
-  expect(Number(math(5, 5, 5, 5, 5).toFixed(2))).toBe(14.17);
-  expect(Number(math(10, 5, 1, 0, 12).toFixed(2))).toBe(-4.83);
-});
