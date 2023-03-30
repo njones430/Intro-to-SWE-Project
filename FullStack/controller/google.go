cbf652f2ca798fb85e47c0e69e8ab36553f418c5
gabrielwest1 <93237913+gabrielwest1@users.noreply.github.com>
2023-03-29 23:08:34 -0400

    Update google.go

--- FullStack/controller/google.go
+++ FullStack/controller/google.go
@@ -1,3 +1,57 @@
+// package controller
+
+// import (
+// 	"context"
+// 	"example/hello/config"
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"strings"
+// )
+
+// func GoogleLogin(res http.ResponseWriter, req *http.Request) {
+// 	googleConfig := config.SetupConfig()
+// 	url := googleConfig.AuthCodeURL("randomstate")
+// 	http.Redirect(res, req, url, http.StatusSeeOther)
+// }
+// func GoogleCallback(res http.ResponseWriter, req *http.Request) {
+// 	state := req.URL.Query()["state"][0]
+// 	if state != "randomstate" {
+// 		fmt.Fprintln(res, "illegal state")
+// 		return
+
+// 	}
+// 	code := req.URL.Query()["code"][0]
+
+// 	googleConfig := config.SetupConfig()
+
+// 	token, err := googleConfig.Exchange(context.Background(), code)
+// 	if err != nil {
+// 		fmt.Fprintln(res, "Code-Token exchange failed")
+// 	}
+
+// 	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+// 	if err != nil {
+// 		fmt.Fprintln(res, "User data fetch failed")
+// 	}
+
+// 	userData, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Fprintln(res, "Json parsing failed")
+// 	}
+// 	//trying to get a string containing only the email
+// 	left, right, isok := strings.Cut(string(userData), "email")
+// 	left, right, isok = strings.Cut(string(right), " \",")
+// 	left, right, isok = strings.Cut(string(left), "\": \"")
+// 	left, right, isok = strings.Cut(string(right), "\"")
+// 	isok = isok
+// 	//EFFICENCY ! 100% !
+// 	fmt.Fprintln(res, left) //unit test returns right string
+
+// 	http.Redirect(res, req, "http://localhost:3000/api/getBudget", http.StatusSeeOther)
+
+// }
+
 package controller
 
 import (
@@ -20,7 +74,6 @@ type Budget struct {
 }
 
 var currentuser = ""
-
 var m = make(map[string]Budget)
 
 func GoogleLogin(res http.ResponseWriter, req *http.Request) {
@@ -60,13 +113,12 @@ func GoogleCallback(res http.ResponseWriter, req *http.Request) {
 	left, right, isok = strings.Cut(string(right), "\"")
 	isok = isok
 	//EFFICENCY ! 100% !
-	//fmt.Fprintln(res, left) //unit test returns right string
 	currentuser = left
 	_, ok := m[currentuser]
 	if !ok {
 
 		m[currentuser] = Budget{
-			Name:         "ufstudent",
+			Name:         currentuser,
 			Tuition:      "10",
 			Rent:         "20",
 			Textbookcost: "30",
@@ -74,7 +126,7 @@ func GoogleCallback(res http.ResponseWriter, req *http.Request) {
 			Grant:        "50",
 		}
 	}
-
+	//fmt.Fprintln(res, left) //unit test returns right string
 	http.Redirect(res, req, "http://localhost:3000/api/getBudget", http.StatusSeeOther)
 
 }
@@ -86,5 +138,5 @@ func ReadDataIncident(res http.ResponseWriter, req *http.Request) {
 	}
 	res.Header().Set("Content-Type", "application/json")
 	res.Write(jsonString)
-	http.Redirect(res, req, "http://localhost:3006", http.StatusSeeOther)
+	//http.Redirect(res, req, "http://localhost:3006", http.StatusSeeOther)
 }
