3712f39099711e73f963f10bb9949d1af93ea5b1
gabrielwest1 <93237913+gabrielwest1@users.noreply.github.com>
2023-04-17 20:43:33 -0400

    Create setupProxy.js

---
+++ FullStack/frontendUI/src/saver/setupProxy.js
@@ -0,0 +1,11 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://localhost:3000', // Replace with the URL of your API server
+//       changeOrigin: true,
+//     })
+//   );
+// };
