57d54ee96a449a5e700d9d8819832fc972e2ea16
gabrielwest1 <93237913+gabrielwest1@users.noreply.github.com>
2023-04-17 20:37:00 -0400

    Update main.go

--- FullStack/main.go
+++ FullStack/main.go
@@ -27,6 +27,7 @@ func main() {
 	http.HandleFunc("/google/login", controller.GoogleLogin)
 	http.HandleFunc("/google/callback", controller.GoogleCallback)
 	http.HandleFunc("/api/getBudget", controller.ReadDataIncident)
+
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
