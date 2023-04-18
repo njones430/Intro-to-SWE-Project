57d086c262f2811b25c7e913de8f4a12a7c43a1d
gabrielwest1 <93237913+gabrielwest1@users.noreply.github.com>
2023-04-17 20:42:04 -0400

    Update testfile.js

--- FullStack/frontendUI/src/saver/testfile.js
+++ FullStack/frontendUI/src/saver/testfile.js
@@ -17,39 +17,68 @@ import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 
 function Testfile() {
-    const [posts,setPosts] = useState({blogs: []})
-    useEffect(()=> {
-        const fetchPostList = async() =>{
-            const {data} = await axios("https://jsonplaceholder.typicode.com/users")
-            
-            setPosts({blogs:data})
-            console.log(data)
-        }
-        fetchPostList()
-    },[setPosts])
-return(
-    <Table striped bordered hover>
-      <thead>
-        <tr>
 
-          <th>Budget Number</th>
-          <th>Budget Name</th>
-          <th>Total Budget</th>
-        </tr> 
-      </thead>
-      <tbody>
-        {
-           posts.blogs && posts.blogs.map((item)=>(
-            <tr key = {item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.address.geo.lat}</td>
-          </tr>
-           )) 
-        }
-      </tbody>
-    </Table>
-)
+//     const [posts,setPosts] = useState({blogs: []})
+//     useEffect(()=> {
+//         const fetchPostList = async() =>{
+//             const {data} = await axios("https://jsonplaceholder.typicode.com/users")
+            
+//             setPosts({blogs:data})
+//             console.log(data)
+//         }
+//         fetchPostList()
+//     },[setPosts])
+// return(
+//     <Table striped bordered hover>
+//       <thead>
+//         <tr>
+
+//           <th>Budget Number</th>
+//           <th>Budget Name</th>
+//           <th>Total Budget</th>
+//         </tr> 
+//       </thead>
+//       <tbody>
+//         {
+//            posts.blogs && posts.blogs.map((item)=>(
+//             <tr key = {item.id}>
+//             <td>{item.id}</td>
+//             <td>{item.name}</td>
+//             <td>{item.address.geo.lat}</td>
+//           </tr>
+//            )) 
+//         }
+//       </tbody>
+//     </Table>
+// )
+const [data, setData] = useState(null);
+
+useEffect(() => {
+  async function fetchData() {
+    const response = await fetch("http://localhost:3000/api/getBudget");
+    const jsonData = await response.json();
+    setData(jsonData);
+  }
+  fetchData();
+}, []);
+
+if (!data) {
+  return <div>Loading data...</div>;
+}
+
+return (
+  
+  <div>
+    <h1>My Data</h1>
+    <p>{data.name}</p>
+    <p>{data.address}</p>
+    <p>{data.rent}</p>
+    <p>{data.textbookcost}</p>
+    <p>{data.payrate}</p>
+    <p>{data.grant}</p>
+    
+  </div>
+);
 }
 
 export default Testfile;
