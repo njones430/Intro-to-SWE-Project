39f57eaa23b2667f7a41a0f118813d1fb3fb8f20
L-Spratt <119978659+L-Spratt@users.noreply.github.com>
2023-04-19 15:02:23 -0400

    Add files via upload

--- FullStack/frontendUI/src/index.js
+++ FullStack/frontendUI/src/index.js
@@ -4,5 +4,12 @@ import App from './App';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import name from './load.txt';
+
+fetch(name)
+ .then(r => r.text())
+ .then(text => {
+    localStorage.setItem("email", text);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
