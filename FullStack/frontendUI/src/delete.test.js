0dc01a985e48e7a57d4ae4bb0ae39b240d50f128
njones430 <87586432+njones430@users.noreply.github.com>
2023-04-19 20:57:01 -0400

    Update delete.test.js

--- FullStack/frontendUI/src/delete.test.js
+++ FullStack/frontendUI/src/delete.test.js
@@ -1,6 +1,6 @@
-const del = require('./create');
+const del = require('./delete');
 
 test('creates a variable, and deletes it', () => {
-  expect(del(5)).toBe(NaN);
-  expect(del(9)).toBe(NaN);
+  expect(del(5)).toBe(null);
+  expect(del(9)).toBe(null);
 });
